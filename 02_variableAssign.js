06f6b58c46a1320e8318d35d843d890ed705438a
jadhavab <153156311+jadhavab@users.noreply.github.com>
2023-12-26 10:29:17 +0530

    javascript variable
    javascript variable

---
+++ 02_variableAssign.js
@@ -0,0 +1,26 @@
+var myFullName
+console.log(myFullName);
+myFullName="Ajay Jadhav";
+console.log(myFullName);
+myFullName="Ajay Bharat Jadhav";
+console.log("complete Name",myFullName);
+
+var myLove="i love only JS";
+console.log(myLove);
+
+var firstName="Ajay";
+var lastName="Jadhav";
+var age="26"
+var address="Arde House Dhamale Nagar Dattawadi Pune"
+var pinCode="431122"
+var gender="Male"
+var isMarried="No"
+console.log("firstName:",firstName);
+console.log("lastName:",lastName);
+console.log("lastName:",age);
+console.log("address:",address);
+console.log("pinCod:",pinCode);
+console.log("gender:",gender);
+console.log(" isMarried:",isMarried);
+
+
---
+++ helloWorld.js
@@ -0,0 +1,8 @@
+console.log("Hello JS I am Ajay" );
+var num="10";
+console.log(num);
+var myName="ajay";
+console.log( myName);
+console.log(" First'Name", myName);
+myName="Ajay Jadhav"
+console.log(" Full name", myName);
